d089ea9d93d2bc30f27fef473301c5cb9001467a
Sammer Alomair <sammeralomair@Sammers-MBP.lan>
2022-05-22 20:09:45 -0700

    pushing changes to token deployment contracts so we can fix missing argument = 2 error

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+contract TweeterCoin is ERC20, AccessControl, ERC20Permit, ERC20Votes {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("TweeterCoin", "TWTR") ERC20Permit("TweeterCoin") {
