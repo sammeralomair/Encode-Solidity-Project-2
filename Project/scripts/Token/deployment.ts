b42060ed26e02902e09ac198fd8602a7fff63a38
Sammer Alomair <sammeralomair@Sammers-MBP.lan>
2022-05-22 17:21:50 -0700

    pushing changes to token deployment contracts so we can fix missing argument = 2 error

--- Project/scripts/Token/deployment.ts
+++ Project/scripts/Token/deployment.ts
@@ -17,28 +17,19 @@ function convertStringArrayToBytes32(array: string[]) {
  * Deploy ballot with environment's wallet as the chairperson.
  */
 async function main() {
-  // Get inputs
-  const proposals = process.argv.slice(2);
-  if (proposals.length < 2) throw new Error("Not enough proposals provided");
 
   // Connect to wallet
   const { signer } = await connectToWallet();
 
   // Deploy wallet
   console.log("Deploying Token contract");
-  console.log("Proposals: ");
-  proposals.forEach((element, index) => {
-    console.log(`Proposal N. ${index + 1}: ${element}`);
-  });
   
   const TokenFactory = new ethers.ContractFactory(
     tokenJson.abi,
     tokenJson.bytecode,
     signer
   );
-  const tokenContract = await TokenFactory.deploy(
-    convertStringArrayToBytes32(proposals)
-  );
+  const tokenContract = await TokenFactory.deploy();
   console.log("Awaiting confirmations");
   await tokenContract.deployed();
   console.log("Completed");
